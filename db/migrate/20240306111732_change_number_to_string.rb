528988ba63071b6d3ea6437a51128f94d861db78
Charlie Wilson <cwhwilson@hotmail.co.uk>
2024-03-06 13:55:34 +0000

    rails new

---
+++ db/migrate/20240306111732_change_number_to_string.rb
@@ -0,0 +1,7 @@
+class ChangeNumberToString < ActiveRecord::Migration[7.1]
+  def change
+    def change
+      add_column :restaurants, :phone_number, :string
+    end
+  end
+end
---
+++ db/migrate/20240306132742_add_restaurant_references.rb
@@ -0,0 +1,4 @@
+class AddRestaurantReferences < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
